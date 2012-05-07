0e65c28bcb1541f3abeb767c9cf57e2441330aa9
Ilya Katz <ilya@rentini.com>
2012-05-07 13:03:18 -0400

    add helpers to the requirements

--- lib/resque-history.rb
+++ lib/resque-history.rb
@@ -1,3 +1,3 @@
 require 'resque'
 require File.expand_path(File.join('resque-history', 'plugins', 'history'), File.dirname(__FILE__))
-require File.expand_path(File.join('resque-history', 'plugins', 'helpers'), File.dirname(__FILE__))
+require File.expand_path(File.join('resque-history', 'helpers', 'helper'), File.dirname(__FILE__))
