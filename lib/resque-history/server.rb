f18428771af63c1ddea82ff842f04c41f8dada97
Ilya Katz <ilyakatz@gmail.com>
2013-04-05 15:26:01 -0400

    started resque2 testing

--- lib/resque-history/server.rb
+++ lib/resque-history/server.rb
@@ -1,6 +1,6 @@
 require 'resque'
 require 'resque/server'
-require 'resque-history'
+#require 'resque-history'
 
 # Extends Resque Web Based UI.
 # Structure has been borrowed from ResqueScheduler.
@@ -48,6 +48,6 @@ module ResqueHistory
 end
 
 Resque.extend ResqueHistory
-Resque::Server.class_eval do
-  include ResqueHistory::Server
-end
+#Resque::Server.class_eval do
+#  include ResqueHistory::Server
+#end
